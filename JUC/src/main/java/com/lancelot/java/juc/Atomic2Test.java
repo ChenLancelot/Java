14368947aacee150d7e0608361cac239292ccd93
ChenLancelot <1303490432@qq.com>
2019-03-01 23:12:57 +0800

    提交代码

---
+++ JUC/src/main/java/com/lancelot/java/juc/Atomic2Test.java
@@ -0,0 +1,44 @@
+package com.lancelot.java.juc;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 
+ * @author Lancelot Chen 
+ * @date 2019年3月1日 下午10:24:05
+ * @Copyright：Lancelot Chen个人所有
+ * @version 1.0 
+ * @Description: TODO
+ */
+public class Atomic2Test {
+
+	public static void main(String[] args) {
+		AtomicDemo ad = new AtomicDemo();
+		for (int i = 0; i < 10; i++) {
+			new Thread(ad).start();
+		}
+		
+	}
+	
+	
+	
+	static class AtomicDemo implements Runnable {
+		/*
+		 * 使用int来做 i++ 操作时出现了重复数据
+		 */
+		private AtomicInteger serialNumber = new AtomicInteger(0);
+		
+		public void run() {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+			}
+			
+			System.out.println(getSerialNumber());
+		}
+		
+		public int getSerialNumber() {
+			return serialNumber.getAndIncrement();
+		}
+	}
+}
---
+++ JUC/src/main/java/com/lancelot/java/juc/CompareAndSwapTest.java
@@ -0,0 +1,51 @@
+package com.lancelot.java.juc;
+/**
+ * 模拟CAS算法
+ * @author Lancelot Chen 
+ * @date 2019年3月1日 下午11:01:47
+ * @Copyright：Lancelot Chen个人所有
+ * @version 1.0 
+ * @Description: TODO
+ */
+public class CompareAndSwapTest {
+
+	public static void main(String[] args) {
+		final CompareAndSwap cas = new CompareAndSwap();
+		
+		for (int i = 0; i < 10; i++) {
+			new Thread(new Runnable() {
+				
+				public void run() {
+					int expectedValue = cas.get();
+					boolean b = cas.compareAndSet(expectedValue, (int)(Math.random() * 101));
+					System.out.println(b);
+				}
+			}).start();
+		}
+	}
+	
+	
+	static class CompareAndSwap {
+		private int value;
+		
+		public synchronized int get() {
+			return value;
+		}
+		
+		public synchronized int compareAndSwap(int expectValue, int newValue) {
+			int oldValue = value;
+			
+			if (oldValue == expectValue) {
+				this.value = newValue;
+			}
+			return oldValue;
+		}
+		
+		//设置
+		public synchronized boolean compareAndSet(int expectedValue, int newValue){
+			return expectedValue == compareAndSwap(expectedValue, newValue);
+		}
+	}
+	
+	
+}
