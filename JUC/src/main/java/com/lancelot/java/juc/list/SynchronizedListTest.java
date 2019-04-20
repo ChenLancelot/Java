4f55200edfba991d8fe7b88254122773ab564e2e
ChenLancelot <1303490432@qq.com>
2019-04-20 23:50:21 +0800

    代码提交

--- JUC/src/main/java/com/lancelot/java/juc/list/SynchronizedListTest.java
+++ JUC/src/main/java/com/lancelot/java/juc/list/SynchronizedListTest.java
@@ -7,7 +7,6 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
